12b6829ac44876c95251a1e21134f0d5a0f6b288
DavisVaughan <mvaugh15@uncc.edu>
2018-04-26 14:15:55 -0400

    Add function validation and functions to cleanup / convert zipline objects

--- R/conversion.R
+++ R/conversion.R
@@ -13,5 +13,73 @@ py_to_r.zipline.assets._assets.Equity <- function(x) {
   )
 
   converted <- lapply(converted, function(.x) {if(is.null(.x)) NA else .x})
-  as.data.frame(converted)
+  tibble::as_tibble(converted)
+}
+
+py_to_r_performance_tibble <- function(x) {
+  df <- reticulate::py_to_r(x)
+  df <- tibble::rownames_to_column(df, "date")
+  x_tbl <- tibble::as_tibble(df)
+
+  if("positions" %in% colnames(x_tbl)) {
+    x_tbl$positions <- py_to_r_positions(x_tbl$positions)
+  }
+
+  if("orders" %in% colnames(x_tbl)) {
+    x_tbl$orders <- py_to_r_positions(x_tbl$orders)
+  }
+
+  if("transactions" %in% colnames(x_tbl)) {
+    x_tbl$transactions <- py_to_r_positions(x_tbl$transactions)
+  }
+
+  x_tbl
+}
+
+py_to_r_positions <- function(positions) {
+
+  lapply(positions, function(positions_on_day) {
+
+    positions_on_day_list <- lapply(positions_on_day, function(position) {
+
+      position_list <- lapply(position, function(element) {
+
+        if (inherits(element, "python.builtin.object")) {
+          py_to_r(element)
+        } else {
+          element
+        }
+
+      })
+
+      if(length(position_list) > 0) {
+        mixed_list_to_tibble(position_list)
+      } else {
+        tibble::tibble()
+      }
+
+    })
+
+    if(length(positions_on_day_list) > 0) {
+      dplyr::bind_rows(positions_on_day_list)
+    } else {
+      positions_on_day_list
+    }
+
+  })
+}
+
+
+mixed_list_to_tibble <- function(x) {
+  x_modified <- lapply(x, function(.x) {
+    if(inherits(.x, "data.frame")) {
+      list(.x)
+    } else if(inherits(.x, "NULL")) {
+      NA
+    } else {
+      .x
+    }
+  })
+
+  tibble::as_tibble(x_modified)
 }
--- R/run.R
+++ R/run.R
@@ -1,5 +1,18 @@
 #' @export
-fly_run_algorithm <- function(start = NULL, end = NULL, capital_base = 10000, bundle = "quandl") {
+fly_run_algorithm <- function(initialize, handle_data, start = NULL, end = NULL, capital_base = 10000, bundle = "quandl", attempt_conversion = TRUE) {
+
+  # Function validation
+  if(!is.function(initialize) && !is.function(handle_data)) {
+    stop("Both `initialize` and `handle_data` must be functions.", call. = FALSE)
+  }
+
+  if(all(rlang::fn_fmls_names(initialize) != "context")) {
+    stop("The argument to the `initialize` function must be named `context`.")
+  }
+
+  if(all(rlang::fn_fmls_names(handle_data) != c("context", "data"))) {
+    stop("The arguments to the `handle_data` function must be named `context` and `data`.", call. = FALSE)
+  }
 
   ensure_r_is_exported()
 
@@ -18,10 +31,18 @@ fly_run_algorithm <- function(start = NULL, end = NULL, capital_base = 10000, bu
     reticulate::source_python("inst/interface.py", convert = FALSE)
   }
 
-  main$py_run(
+  performance <- main$py_run(
+    fly_initialize = initialize,
+    fly_handle_data = handle_data,
     start = start,
     end = end,
     capital_base = capital_base,
     bundle = bundle
   )
+
+  if(attempt_conversion) {
+    py_to_r_performance_tibble(performance)
+  } else {
+    performance
+  }
 }
